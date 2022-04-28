1b79fe3492e612b05b2ea1cff0b1a87813fb89c1
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-04-27 23:54:48 -0300

    Add files via upload

---
+++ pacote_dados_R.R
@@ -0,0 +1,38 @@
+
+# Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data: 27/04/22 ---------------------------------------------------------------------------------------------------------------------------
+
+# Baixar pacote ----------------------------------------------------------------------------------------------------------------------------
+
+library(dados)
+
+?dados # Pesquisar sobre o pacote no R
+
+# Descrição do pacote ----------------------------------------------------------------------------------------------------------------------
+
+### Este pacote traduz os seguintes conjuntos de dados: 'airlines', 'airports', 
+### 'ames_raw', 'AwardsManagers', 'babynames', 'Batting', 'diamonds', 'faithful', 
+### 'fueleconomy', 'Fielding', 'flights', 'gapminder', 'gss_cat', 'iris', 
+### 'Managers', 'mpg', 'mtcars', 'atmos', 'penguins', 'People, 'Pitching', 
+### 'pixarfilms','planes', 'presidential', 'table1', 'table2', 'table3', 'table4a',
+### 'table4b', 'table5', 'vehicles', 'weather', 'who'. 
+
+# Visualizando alguns conjuntos de dados ---------------------------------------------------------------------------------------------------
+
+dados::aeroportos
+View(aeroportos)
+
+dados::casas
+View(casas)
+
+dados::companhias_aereas
+View(companhias_aereas)
+
+dados::clima
+View(clima)
+
+dados::dados_oms
+View(dados_oms)
+
+dados::dados_starwars
+View(dados_starwars)
